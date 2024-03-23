a00e0de92ea4a02abf4ba98aa28aa4ff42912203
RaphaelSantos2023 <raphaelsantosoliveira4@gmail.com>
2024-03-22 23:39:16 -0300

    0,3

---
+++ model/Charact/Player.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Player {
+    
+    private float Money;
+    private int hp;
+    private String name;
+    private ArrayList Equipamentos = new ArrayList<Equipamento>();
+}
--- model/knife.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class knife extends Equipamento{
-
-    public knife(){
-        this.setName("knife");
-        this.setDamege(1.2f);
-    }
-}
