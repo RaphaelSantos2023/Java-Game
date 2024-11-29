5652244779ba8f9d38e6f19003bd83c0e63aba1d
joaoqys <joaobacelar02@gmaill.com>
2024-11-29 09:59:06 -0300

    Classe goblin - Raphael

---
+++ model/Charact/Goblin.java
@@ -0,0 +1,19 @@
+package model.Charact;
+
+import model.Equip.knife;
+
+public class Goblin extends Character{
+    
+    public Goblin(int level){
+        this.setHP(10);
+        this.setLevel(level);
+        this.setName("Goblin");
+        this.setWeapon(new knife());
+        this.setStr(3);
+        this.setDex(7);
+        this.setDiceValue(10);
+        this.setImagem("image\\Goblin.jpeg");
+        this.setBackGround("image\\FLOREST_Background.png");
+        this.levelChange();
+    }
+}
